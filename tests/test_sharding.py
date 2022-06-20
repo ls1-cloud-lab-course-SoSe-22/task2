70583d12637671eb6a639710733841f3fa588520
“lief37” <alex.pierre20@yahoo.fr>
2022-06-20 21:26:39 +0200

    Fixing test termination on failure

--- tests/test_sharding.py
+++ tests/test_sharding.py
@@ -18,12 +18,18 @@ def main() -> None:
         for i in range(1, 21):
             client_ret = run_client(1026, "PUT", i, 1000, 1025, 0)
             if client_ret !=0:
+                master_proc.terminate()
+                server_proc_one.terminate()
+                server_proc_two.terminate()
                 sys.exit(1)
             sleep(3)
 
         for i in range(1, 21):
             client_ret = run_client(1026, "GET", i, 1000, 1025, 0)
             if client_ret !=0:
+                master_proc.terminate()
+                server_proc_one.terminate()
+                server_proc_two.terminate()
                 sys.exit(1)
             sleep(3)
         
@@ -39,6 +45,8 @@ def main() -> None:
             sleep(3)
         
         if not get_failed:
+            master_proc.terminate()
+            server_proc_one.terminate()
             sys.exit(1)
 
         info(f"ran all clients successfully")
--- tests/test_two_shards.py
+++ tests/test_two_shards.py
@@ -18,12 +18,18 @@ def main() -> None:
         for i in range(1, 21):
             client_ret = run_client(1026, "PUT", i, 1000, 1025, 0)
             if client_ret !=0:
+                master_proc.terminate()
+                server_proc_one.terminate()
+                server_proc_two.terminate()
                 sys.exit(1)
             sleep(3)
 
         for i in range(1, 21):
             client_ret = run_client(1026, "GET", i, 1000, 1025, 0)
             if client_ret !=0:
+                master_proc.terminate()
+                server_proc_one.terminate()
+                server_proc_two.terminate()
                 sys.exit(1)
             sleep(3)
         
